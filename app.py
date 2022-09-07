5ae3c5155560a173e10e840252b7a05da2030831
NeroUcH <hin1997101@gmail.com>
2022-09-07 15:59:25 +0800

    feat: get doc url with threading

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import time
 import parser
 import argparse
+import threading
 
 '''
 cchardet, lxml 都是可以加速 bs4 的套件
@@ -39,13 +40,14 @@ page_url_list = []
 doc_list = []
 webdriver_path = '/Users/nerohin/Downloads/chromedriver' # chromedriver 路徑
 requests_session = requests.Session()
+ncku_url = 'https://www.csie.ncku.edu.tw'
+thread_list = []
 
 # 把所有公告頁面的連結都抓下來
 def get_announce_page(start_page: int, end_page: int):
 
     base_url = 'https://www.csie.ncku.edu.tw/ncku_csie/announce/news/1000'
     urls = [f'{base_url}?Infolist_page={page}' for page in range(start_page, end_page+1,1)]  # 1~100頁的網址清單，100 頁是到 2020-08-17
-    print(urls)
     for url in tqdm(urls):
 
         web = requests_session.get(url)
@@ -56,26 +58,27 @@ def get_announce_page(start_page: int, end_page: int):
         for link in soup.find_all('a'):
             if link.get('href') is not None: # 有連結
                 if re.match(r'/ncku_csie/announce/view/\d+', link.get('href')): # 有 /ncku_csie/announce/view/ 的連結
-                    page_url_list.append('https://www.csie.ncku.edu.tw' + link.get('href')) # 把連結加到 page_url_list
+                    page_url_list.append(link.get('href')) # 把連結加到 page_url_list
 
 # 把公告裡有 Download 的連結都抓下來
-def get_doc_url(list: list):
-  
-    for index,page in enumerate(iterable=tqdm(list)):
-        web = requests_session.get(page)
-        web.encoding = 'utf-8' # 設定編碼
-        page_soup = BeautifulSoup(web.text, 'lxml', parse_only=SoupStrainer(class_="odd")) # 目前檔案下載都會在 odd class 裡面
-        title = page_soup.find('td').text
-        print(f'第 {index} 篇公告，文字標題: {title}')
-        for link in page_soup.find_all('a'):
-            if re.match(r'/ncku_csie/Attachment/Download/', link.get('href')): # 有 Download 的連結
-                doc_list.append('https://www.csie.ncku.edu.tw' + link.get('href')) # 把連結加到 doc_list
+def get_doc_url(page: str):
+
+    page = ncku_url + page
+    web = requests_session.get(page)
+    web.encoding = 'utf-8' # 設定編碼
+    page_soup = BeautifulSoup(web.text, 'lxml', parse_only=SoupStrainer(class_="odd")) # 目前檔案下載都會在 odd class 裡面
+    title = page_soup.find('td').text
+    print(f'文字標題: {title}')
+    for link in page_soup.find_all('a'):
+        if re.match(r'/ncku_csie/Attachment/Download/', link.get('href')): # 有 Download 的連結
+            doc_list.append(link.get('href')) # 把連結加到 doc_list
 
 # 下載檔案
 ## TODO 下載到特定資料夾和名字
 
 def download_doc(list: list):     
     for doc in tqdm(doc_list):
+        doc = ncku_url + doc
         driverPath = webdriver_path
         driver = webdriver.Chrome(driverPath)
         driver.get(doc) # 進入網頁
@@ -83,19 +86,30 @@ def download_doc(list: list):
         driver.close() # 關閉瀏覽器
 
 start_time = time.time()
-print(f'抓取 {start_page} ~ {end_page} 頁的公告')
-print(f'開始時間: {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+print(f'抓取 {start_page} ~ {end_page} 頁的公告\n')
 
 get_announce_page(start_page=start_page, end_page=end_page)
-get_doc_url(list=page_url_list)
+print(f'共抓取 {len(page_url_list)} 篇公告\n')
+
+# get doc url with threading
+
+for index, page in enumerate(page_url_list):
+    page_thread = threading.Thread(target=get_doc_url, args=(page,))
+    thread_list.append(page_thread)
+
+# 開始
+for thread in thread_list:
+    thread.start()
+
+# 等待所有子執行緒結束
+for thread in thread_list:
+    thread.join()
 
 if download == True:
     download_doc(list=doc_list)
-    print(f'共抓取 {len(doc_list)} 個檔案')
+    print(f'共抓取 {len(doc_list)} 個檔案\n')
 
-
-print(f'結束時間: {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
-print(f'共抓取 {len(page_url_list)} 篇公告')
+print(f'共抓取 {len(doc_list)} 個檔案\n')
 end_time = time.time()
 print(f'所用時間: {time.time() - start_time}')  
 
